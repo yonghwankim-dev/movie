b0d9dd0a668eab35c63427f94a88f1114cc0dc64
yonghwan <yonghwankim.dev@gmail.com>
2022-03-13 12:57:32 +0900

    좌석 클릭 이벤트 구현환료

--- person_seat.js
+++ person_seat.js
@@ -1,6 +1,7 @@
 let current_person = 0;
 let activated_seat = 0;
 
+// 좌석 생성
 const createSeatMap = function(){
     const seat_row = [null,"A","B","C","D","E","F","G","I"];
     const tbody = document.querySelector("#showMapTableBody");
@@ -17,6 +18,8 @@ const createSeatMap = function(){
             const td = document.createElement("td");
             const a = document.createElement("a");
 
+            td.className = "seat";
+
             a.href = "#seat/"+seat_row[i]+j;
             a.textContent = j;
 
@@ -28,31 +31,69 @@ const createSeatMap = function(){
     }
 }
 
+// 선택 완료
+const completeSelect = function(){
+    const seats = document.querySelectorAll("#showMapTableBody tr td.seat");
+    const person_10_num = document.querySelector("#person_10 .txt_num");    // 청소년 인원 * 10,000
+    const person_20_num = document.querySelector("#person_20 .txt_num");    // 성인 인원 * 14,000
+    const person_12_num = document.querySelector("#person_12 .txt_num");    // 노약자 인원 * 7,000
+    const total_price = document.querySelector(".total_price dd strong");
+    let sum = 0;
+
+    seats.forEach((seat)=>{
+        if(!seat.classList.contains("active"))
+        {
+            seat.classList.add("no_select");
+        }
+    });
+
+    // 영화 요금 합계
+    sum = Number(person_10_num.textContent)*10000 + Number(person_20_num.textContent)*14000 + Number(person_12_num.textContent)*7000;
+    total_price.textContent = sum;
+}
+
+// 선택 완료 해제
+const clearSelect = function(){
+    const seats = document.querySelectorAll("#showMapTableBody tr td.seat");
+    const total_price = document.querySelector(".total_price dd strong");
+
+    seats.forEach((seat)=>{
+        if(seat.classList.contains("no_select"))
+        {
+            seat.classList.remove("no_select");
+        }
+    });
+
+    total_price.textContent = 0;
+}
+
+// 좌석 선택
 const activeSeat = function(e){
     
-    if(current_person===0)
+    if(current_person===0 || e.currentTarget.classList.contains("reserved") || e.currentTarget.classList.contains("no_select"))
     {
         return;
     }
     
-
     if(e.currentTarget.classList.contains("active"))
     {
         e.currentTarget.classList.remove("active");
         activated_seat -= 1;
+        clearSelect();
     }
     else
     {
-        if(current_person===activated_seat)
-        {
-            alert("선택 완료");
-            return;
-        }
-
         e.currentTarget.classList.add("active");
         activated_seat += 1;
+
+        if(current_person===activated_seat)
+        {
+            completeSelect();
+            return;
+        }
     }
 };
+
 createSeatMap();
 
 const seats = document.querySelectorAll("#showMapTableBody td");
@@ -79,7 +120,7 @@ const person10_mins_click = function(){
     {
         return;
     }
-    const mins_num = Number(txt_num.textContent) + 1;
+    const mins_num = Number(txt_num.textContent) -1;
     current_person -= 1;
     txt_num.textContent = mins_num;
 }

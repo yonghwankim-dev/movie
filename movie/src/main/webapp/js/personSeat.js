aae28f3dd6865b8c0826208c8b3a550757620bbf
yonghwankim-dev <yonghwankim.dev@gmail.com>
2022-05-19 13:34:17 +0900

    개인좌석 페이지 자바스크립트 코드 정리 및 개선

--- movie/src/main/webapp/js/personSeat.js
+++ movie/src/main/webapp/js/personSeat.js
@@ -1,17 +1,57 @@
-let current_person = 0;
-let activated_seat = 0;
+let current_person = 0;	// 현재 선택한 사람수
+let activated_seat = 0; // 선택한 좌석수
 
-// 선택 완료
-const completeSelect = function(){
-    const seats = document.querySelectorAll("#showMapTableBody tr td.seat");
-	const actived_seat_input = document.querySelectorAll("#showMapTableBody tr td.seat.active input");
-    const person_10_num = document.querySelector("#person_10 .txt_num");    // 청소년 인원 * 10,000
-    const person_20_num = document.querySelector("#person_20 .txt_num");    // 성인 인원 * 14,000
-    const person_12_num = document.querySelector("#person_12 .txt_num");    // 노약자 인원 * 7,000
-    const total_price = document.querySelector(".total_price dd strong");
-	const input_price = document.querySelector("#input_price");
-	const input_seat =document.querySelector("#input_seat");
-		
+const seats = document.querySelectorAll("#showMapTableBody tr td.seat");
+const seats_td = document.querySelectorAll("#showMapTableBody td");
+const actived_seat_input = document.querySelectorAll("#showMapTableBody tr td.seat.active input");
+const person_10_num = document.querySelector("#person_10 .txt_num");    // 청소년 인원 * 10,000
+const person_20_num = document.querySelector("#person_20 .txt_num");    // 성인 인원 * 14,000
+const person_12_num = document.querySelector("#person_12 .txt_num");    // 노약자 인원 * 7,000
+const total_price = document.querySelector(".total_price dd strong");	// 좌석값 전체합계
+const input_price = document.querySelector("#input_price");				
+const input_seat =document.querySelector("#input_seat");
+
+const input_person_10 = document.querySelector("#input_person_10"); // 청소년 input 태그
+const input_person_20 = document.querySelector("#input_person_20"); // 성인 input 태그
+const input_person_12 = document.querySelector("#input_person_12"); // 노약자 input 태그
+
+// 청소년/성인/노약자 플러스-마이너스 버튼
+const person10_plus_btn = document.querySelector("#person_10 .btn_plus");
+const person10_mins_btn = document.querySelector("#person_10 .btn_mins");
+const person20_plus_btn = document.querySelector("#person_20 .btn_plus");
+const person20_mins_btn = document.querySelector("#person_20 .btn_mins");
+const person12_plus_btn = document.querySelector("#person_12 .btn_plus");
+const person12_mins_btn = document.querySelector("#person_12 .btn_mins");
+
+// 인원 증가 
+const increasePerson = function(){
+	current_person++;
+}
+// 인원 감소
+const decreasePerson = function(){
+	current_person--;
+}
+
+// 좌석 상태(선택/선택불가)초기화
+const resetSeatStatus = function(){
+    seats.forEach((seat)=>{
+        if(seat.classList
+               .contains("no_select")){
+            seat.classList.remove("no_select");
+        }
+		if(seat.classList.contains("active")){
+			seat.classList.remove("active");
+		}
+    });
+
+	activated_seat = 0;
+	total_price.textContent = input_price.value 
+                            = input_price.textContent
+							= 0;	
+}
+
+// 좌석 선택 완료
+const completeSelect = function(){		
     let sum = 0;
 
 	const step1 = ()=>{
@@ -43,146 +83,154 @@ const completeSelect = function(){
 		+ Number(person_20_num.value)*14000
 		+ Number(person_12_num.value)*7000;
 		
-    total_price.textContent = sum;
-	input_price.value = sum;
-	input_price.textContent = sum;
+    total_price.textContent = input_price.value 
+                            = input_price.textContent 
+                            = sum;
 }
 
 // 선택 완료 해제
 const clearSelect = function(){
-    const seats = document.querySelectorAll("#showMapTableBody tr td.seat");
     const total_price = document.querySelector(".total_price dd strong");
 
     seats.forEach((seat)=>{
-        if(seat.classList.contains("no_select"))
-        {
+        if(seat.classList
+               .contains("no_select")){
             seat.classList.remove("no_select");
         }
     });
-
+	
     total_price.textContent = 0;
 }
 
 // 좌석 선택
 const activeSeat = function(e){
     
-    if(current_person===0 || e.currentTarget.classList.contains("reserved") || e.currentTarget.classList.contains("no_select"))
-    {
+    if(current_person===0 
+    || e.currentTarget.classList.contains("reserved") 
+    || e.currentTarget.classList.contains("no_select")){
         return;
     }
     
-    if(e.currentTarget.classList.contains("active"))
-    {
+    if(e.currentTarget
+        .classList
+        .contains("active")){
         e.currentTarget.classList.remove("active");
-        activated_seat -= 1;
-        clearSelect();
-    }
-    else
-    {
-        e.currentTarget.classList.add("active");
-        activated_seat += 1;
+        activated_seat--;
 
-        if(current_person===activated_seat)
-        {
+		clearSelect();
+    }
+    else{
+        e.currentTarget.classList.add("active");
+        activated_seat++;
+
+        if(current_person===activated_seat){
             completeSelect();
             return;
         }
     }
 };
 
-const seats = document.querySelectorAll("#showMapTableBody td");
-seats.forEach((item)=>{
+////////////////////////////////////////////////////////////////////////////
+
+// 청소년 플러스(+) 버튼 클릭 이벤트
+const person10_plus_click = function(){    
+    const plus_num = Number(person_10_num.value) + 1;
+
+    person_10_num.value = input_person_10.value = plus_num;
+	increasePerson();
+	
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+
+// 청소년 마이너스(-) 버튼 클릭 이벤트
+const person10_mins_click = function(){
+    const mins_num = Number(person_10_num.value) -1;
+
+    if(person_10_num.value==='0'){
+        return;
+    }
+    
+    person_10_num.value = input_person_10.value = mins_num;
+	decreasePerson();
+	
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+
+// 성인 플러스(+) 버튼 클릭 이벤트
+const person20_plus_click = function(){	
+    const plus_num = Number(person_20_num.value) + 1;
+    
+    person_20_num.value= input_person_20.value = plus_num;
+    increasePerson();
+
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+// 성인 마이너스(-) 버튼 클릭 이벤트
+const person20_mins_click = function(){
+	const minus_num = Number(person_20_num.value) - 1;
+	
+    if(person_20_num.value==='0'){
+        return;
+    }
+    
+    person_20_num.value = input_person_20.value = minus_num;
+	decreasePerson();
+	
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+
+// 시니어 플러스(+) 버튼 클릭 이벤트
+const person12_plus_click = function(){
+    const plus_num = Number(person_12_num.value) + 1;
+
+    person_12_num.value = input_person_12.value = plus_num;
+	increasePerson();
+	
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+
+// 시니어 마이너스(-) 버튼 클릭 이벤트
+const person12_mins_click = function(){
+	const minus_num = Number(person_12_num.value) - 1;
+	
+    if(person_12_num.value==='0')
+    {
+        return;
+    }
+    
+    person_12_num.value= input_person_12.value = minus_num;
+	
+	decreasePerson();
+	
+	if(activated_seat >= 1){
+		resetSeatStatus();	
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+// 좌석 클릭 이벤트 등록
+seats_td.forEach((item)=>{
     item.addEventListener("click",activeSeat);
 });
 
-
-
-
-
-////////////////////////////////////////////////////////////////////////////
-// 청소년 +/-버튼 클릭
-const person10_plus_click = function(){
-    const txt_num = document.querySelector("#person_10 .txt_num");
-	const input_person_10 = document.querySelector("#input_person_10");
-    const plus_num = Number(txt_num.value) + 1;
-
-    current_person += 1;
-    txt_num.value = plus_num;
-	input_person_10.value = plus_num;
-}
-const person10_mins_click = function(){
-	const input_person_10 = document.querySelector("#input_person_10");
-    const txt_num = document.querySelector("#person_10 .txt_num");
-
-    if(txt_num.value==='0')
-    {
-        return;
-    }
-    const mins_num = Number(txt_num.value) -1;
-    current_person -= 1;
-    txt_num.value = mins_num;
-	input_person_10.value = mins_num;
-}
-
-// 성인 +/- 버튼 클릭
-const person20_plus_click = function(){
-	const input_person_20 = document.querySelector("#input_person_20");
-    const txt_num = document.querySelector("#person_20 .txt_num");
-    const plus_num = Number(txt_num.value) + 1;
-
-    current_person += 1;
-    txt_num.value= plus_num;
-	input_person_20.value = plus_num;
-}
-const person20_mins_click = function(){
-	const input_person_20 = document.querySelector("#input_person_20");
-    const txt_num = document.querySelector("#person_20 .txt_num");
-    if(txt_num.value==='0')
-    {
-        return;
-    }
-
-    const minus_num = Number(txt_num.value) - 1;
-    current_person -= 1;
-    txt_num.value = minus_num;
-	input_person_20.value = minus_num;
-}
-
-// 시니어 +/- 버튼 클릭
-const person12_plus_click = function(){
-	const input_person_12 = document.querySelector("#input_person_12");
-    const txt_num = document.querySelector("#person_12 .txt_num");
-    const plus_num = Number(txt_num.value) + 1;
-
-    current_person += 1;
-    txt_num.value = plus_num;
-	input_person_12.value = plus_num;
-}
-const person12_mins_click = function(){
-	const input_person_12 = document.querySelector("#input_person_12");
-    const txt_num = document.querySelector("#person_12 .txt_num");
-    if(txt_num.value==='0')
-    {
-        return;
-    }
-    const minus_num = Number(txt_num.value) - 1;
-    current_person -= 1;
-    txt_num.value= minus_num;
-	input_person_12.value = minus_num;
-}
-
-const person10_plus = document.querySelector("#person_10 .btn_plus");
-const person10_mins = document.querySelector("#person_10 .btn_mins");
-const person20_plus = document.querySelector("#person_20 .btn_plus");
-const person20_mins = document.querySelector("#person_20 .btn_mins");
-const person12_plus = document.querySelector("#person_12 .btn_plus");
-const person12_mins = document.querySelector("#person_12 .btn_mins");
-person10_plus.addEventListener("click",person10_plus_click);
-person10_mins.addEventListener("click",person10_mins_click);
-person20_plus.addEventListener("click",person20_plus_click);
-person20_mins.addEventListener("click",person20_mins_click);
-person12_plus.addEventListener("click",person12_plus_click);
-person12_mins.addEventListener("click",person12_mins_click);
+// 청소년/성인/노약자 플러스-마이너스 버튼, 이벤트 등록 
+person10_plus_btn.addEventListener("click",person10_plus_click);
+person10_mins_btn.addEventListener("click",person10_mins_click);
+person20_plus_btn.addEventListener("click",person20_plus_click);
+person20_mins_btn.addEventListener("click",person20_mins_click);
+person12_plus_btn.addEventListener("click",person12_plus_click);
+person12_mins_btn.addEventListener("click",person12_mins_click);
 
 ////////////////////////////////////////////////////////////////////////////////
 
